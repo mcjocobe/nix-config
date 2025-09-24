d1f32739931006c831052bb2cfadf652b17bff68
Jose Colomer <joseluis.colomer@tiko.energy>
2025-09-24 18:24:09 +0200

    Update to nix 25.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Example Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
